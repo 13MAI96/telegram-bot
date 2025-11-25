b4f936214ea2fb6c97c721e169dba4a712a56bdf
Marcelo Iglesias - Linux A515 <marceloagustiniglesias@gmail.com>
2025-11-25 11:38:40 -0300

    Getting server temp public IP

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,17 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
 
+  constructor(){
+    this.getPublicIp()
+  }
+
   getHello(): string {
     return 'Hello world!'
   }
+
+  private getPublicIp = async () => {
+  const req = await fetch("https://api.ipify.org?format=json");
+  const res = await req.json()
+  console.log(res.ip);
+}
 }
